d7ce454a06c932aa572f1501f3b89262be6d7038
misi9170 <michael.sinner@nrel.gov>
2026-02-16 11:17:13 -0700

    Input file description; plot price threshold

--- examples/natural_gas_plant_comparison/plot_outputs.py
+++ examples/natural_gas_plant_comparison/plot_outputs.py
@@ -5,7 +5,7 @@ import numpy as np
 from hercules import HerculesOutput
 
 
-def plot_outputs():
+def plot_outputs(price_threshold=10.0):
     # Read the Hercules output file using HerculesOutput
     ho = HerculesOutput("outputs/hercules_output.h5")
 
@@ -35,73 +35,85 @@ def plot_outputs():
         linewidth=0.5,
     )
 
-    # Process and plot the day ahead top and bottom 4 and 1
-    # for each midnight to midnight (UTC) period
-    df_reduced = df[["time_utc", "external_signals.lmp_da"]].copy()
-
-    # Create an hourly version for the DA LMP (drop all periods that aren't on an hour)
-    df_hr = df_reduced[df_reduced["time_utc"].dt.minute == 0].copy(deep=True)
-
-    # Extract date and hour for use in pivot_table
-    df_hr["date"] = df_hr["time_utc"].dt.date
-    df_hr["hour"] = df_hr["time_utc"].dt.hour
-
-    df_hourly = df_hr.pivot_table(
-        values="external_signals.lmp_da",
-        index="date",
-        columns="hour",
-        aggfunc="first",  # Use first value if multiple entries per hour
-    )
-    day_hour = df_hourly.to_numpy()
-    sorted_day_hour = np.sort(day_hour, axis=1)
-    df_hourly["B4"] = sorted_day_hour[:, 3]
-    df_hourly["T4"] = sorted_day_hour[:, -4]
-    df_hourly["B1"] = sorted_day_hour[:, 0]
-    df_hourly["T1"] = sorted_day_hour[:, -1]
-
-    df = df.merge(
-        df_hourly[["B4", "T4", "B1", "T1"]],
-        left_on=df["time_utc"].dt.date,
-        right_on=df_hourly.index,
-        how="left",
-    ).ffill()
-
     ax.fill_between(
-        df["time"],
-        df["T4"],
-        500,
-        label="Main discharge price (daily)",
-        color="C2",
-        alpha=0.3,
+        [df["time"].min(), df["time"].max()],
+        [price_threshold, price_threshold],
+        [-50, -50],
+        label="Below price threshold",
+        color="C3",
+        linestyle="-",
+        linewidth=1.0,
+        alpha=0.1,
         edgecolor="None",
     )
-    ax.fill_between(
-        df["time"],
-        df["T1"],
-        500,
-        label="High discharge price (daily)",
-        color="C2",
-        alpha=0.5,
-        edgecolor="None",
-    )
-    ax.fill_between(
-        df["time"],
-        -500,
-        df["B4"],
-        label="Main charge price (daily)",
-        color="C1",
-        alpha=0.3,
-        edgecolor="None",
-    )
-    ax.fill_between(
-        df["time"],
-        -500,
-        df["B1"],
-        label="Low charge price (daily)",
-        color="C1",
-        alpha=0.5,
-        edgecolor="None",
-    )
+
+    # # Process and plot the day ahead top and bottom 4 and 1
+    # # for each midnight to midnight (UTC) period
+    # df_reduced = df[["time_utc", "external_signals.lmp_da"]].copy()
+
+    # # Create an hourly version for the DA LMP (drop all periods that aren't on an hour)
+    # df_hr = df_reduced[df_reduced["time_utc"].dt.minute == 0].copy(deep=True)
+
+    # # Extract date and hour for use in pivot_table
+    # df_hr["date"] = df_hr["time_utc"].dt.date
+    # df_hr["hour"] = df_hr["time_utc"].dt.hour
+
+    # df_hourly = df_hr.pivot_table(
+    #     values="external_signals.lmp_da",
+    #     index="date",
+    #     columns="hour",
+    #     aggfunc="first",  # Use first value if multiple entries per hour
+    # )
+    # day_hour = df_hourly.to_numpy()
+    # sorted_day_hour = np.sort(day_hour, axis=1)
+    # df_hourly["B4"] = sorted_day_hour[:, 3]
+    # df_hourly["T4"] = sorted_day_hour[:, -4]
+    # df_hourly["B1"] = sorted_day_hour[:, 0]
+    # df_hourly["T1"] = sorted_day_hour[:, -1]
+
+    # df = df.merge(
+    #     df_hourly[["B4", "T4", "B1", "T1"]],
+    #     left_on=df["time_utc"].dt.date,
+    #     right_on=df_hourly.index,
+    #     how="left",
+    # ).ffill()
+
+    # ax.fill_between(
+    #     df["time"],
+    #     df["T4"],
+    #     500,
+    #     label="Main discharge price (daily)",
+    #     color="C2",
+    #     alpha=0.3,
+    #     edgecolor="None",
+    # )
+    # ax.fill_between(
+    #     df["time"],
+    #     df["T1"],
+    #     500,
+    #     label="High discharge price (daily)",
+    #     color="C2",
+    #     alpha=0.5,
+    #     edgecolor="None",
+    # )
+    # ax.fill_between(
+    #     df["time"],
+    #     -500,
+    #     df["B4"],
+    #     label="Main charge price (daily)",
+    #     color="C1",
+    #     alpha=0.3,
+    #     edgecolor="None",
+    # )
+    # ax.fill_between(
+    #     df["time"],
+    #     -500,
+    #     df["B1"],
+    #     label="Low charge price (daily)",
+    #     color="C1",
+    #     alpha=0.5,
+    #     edgecolor="None",
+    # )
     ax.set_ylim([-50, 50])
     ax.set_ylabel("Price [$/MWh]")
     ax.set_xlim([0, df["time"].max()])
